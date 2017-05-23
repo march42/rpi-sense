543745095647e89f0f7d5e6dc5e8b28110ea5624
march42 <marchefter@march42.net>
2017-05-23 17:58:48 +0200

    code reorg TWI code
    I2C registers[256] buffer for all data
    I2C register address checking and ACK/NACK
    I2C register address enum REG_ADDR
    optimized TWI receive/transmit
    ISR code bug fix - removed RETI to execute POPs
    assembly bug fixes in write_data - parameter use registers r25--
    corrected enum le_key values to bit number (TYPE==r18 in write_data loop)
    fixed read_data assembly code - needs a lot more optimization

--- main.c
+++ main.c
@@ -54,85 +54,117 @@
 extern void draw_loop();
 extern void clear_gain(void);
 extern void delay(uint8_t ticks);
-extern void write_data(uint32_t data, char type);
 extern void check_keys(void);
-
-/*	seems to be unused, maybe was intend to be I2C register addresses
-enum REG_ADDR {
-	REG_ID = 192,
-	REG_CFG_LOW,
-	REG_CFG_HIGH,
-};
-*/
 
 /*	bit count values for LED2472G shift register
 **	LED_LE must rise after transmitting n-th bit
 **	TODO: use le_key for write_data and read_data
 */
 typedef enum {
-	DAT_LATCH = 22,
-	CONF_WRITE = 20,
-	CONF_READ  = 18,
-	GAIN_WRITE = 16,
-	GAIN_READ = 14,
-	DET_OPEN = 13,
+	DAT_LATCH = 1,
+	CONF_WRITE = 3,
+	CONF_READ  = 5,
+	GAIN_WRITE = 7,
+	GAIN_READ = 9,
+	DET_OPEN = 11,
 	DET_SHORT = 12,
-	DET_OPEN_SHORT = 11,
-	THERM_READ = 10,
+	DET_OPEN_SHORT = 13,
+	THERM_READ = 14,
 } le_key;
+extern void write_data(uint32_t data, le_key type);
+extern uint32_t read_data(le_key type);
 
-volatile uint8_t pixels[] = {
-	// line 1
+/*	I2C register addresses
+*/
+enum REG_ADDR {
+	REG_PIXELS=0, REG_PIXELS_MAX=((8*8*3)-1),
+	/*	unused - left from initial version
+	REG_ID = 192,
+	REG_CFG_LOW,
+	REG_CFG_HIGH,
+	*/
+	REG_PINA=0xE0, REG_DDRA, REG_PORTA,
+	REG_PINB, REG_DDRB, REG_PORTB,
+	REG_PINC, REG_DDRC, REG_PORTC,
+	REG_PIND, REG_DDRD, REG_PORTD,
+	REG_MCUSR, REG_MCUCR,
+	REG_PORTCR, REG_PRR,
+	REG_WAI=0xF0, REG_VER=0xF1,
+	REG_KEYS=0xF2,
+	REG_EE_WP=0xF3,
+	REG_WDTCSR=0xF4, REG_SMCR=0xf5,
+	REG_WDC=0xF6,
+};
+
+#define I2C_ID	's'
+#ifndef	NDEBUG
+#	define VERSION	0xA1	/* debug version */
+#else
+#	define VERSION	0x01	/* release version */
+#endif
+
+volatile uint8_t registers[0x100] __attribute__ ((aligned (0x100))) = {
+	//	offset 0x00 - line 1
 	0x1F, 0x1F, 0x1F, 0x1F, 0x14, 0x03, 0x00, 0x00,	// red 1-8
 	0x00, 0x00, 0x03, 0x12, 0x1F, 0x1F, 0x1F, 0x1F,	// green 1-8
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07,	// blue 1-8
-	// line 2
+	//	offset 0x18 - line 2
 	0x1F, 0x1F, 0x1F, 0x12, 0x03, 0x00, 0x00, 0x00,	// red 9-16
 	0x00, 0x04, 0x14, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F,	// green 9-16
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x08, 0x1D,	// blue 9-16
-	// line 3
+	//	offset 0x30 - line 3
 	0x1F, 0x1F, 0x11, 0x02, 0x00, 0x00, 0x00, 0x00,	// red 17-24
 	0x05, 0x15, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x0B,	// green 17-24
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x1F, 0x1F,	// blue 17-24
-	// line 4
+	//	offset 0x48 - line 4
 	0x1F, 0x0F, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00,	// red 25-32
 	0x17, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x0A, 0x00,	// green 25-32
 	0x00, 0x00, 0x00, 0x00, 0x0A, 0x1F, 0x1F, 0x1F,	// blue 25-32
-	// line 5
+	//	offset 0x60 - line 5
 	0x0E, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03,	// red 33-40
 	0x1F, 0x1F, 0x1F, 0x1F, 0x1D, 0x08, 0x00, 0x00,	// green 33-40
 	0x00, 0x00, 0x01, 0x0B, 0x1F, 0x1F, 0x1F, 0x1F,	// blue 33-40
-	// line 6
+	//	offset 0x78 - line 6
 	0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x14,	// red 41-48
 	0x1F, 0x1F, 0x1F, 0x1B, 0x07, 0x00, 0x00, 0x00,	// green 41-48
 	0x00, 0x01, 0x0C, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F,	// blue 41-48
-	// line 7
+	//	offset 0x90 - line 7
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x15, 0x1F,	// red 49-56
 	0x1F, 0x1F, 0x19, 0x06, 0x00, 0x00, 0x00, 0x00,	// green 49-56
 	0x02, 0x0E, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x12,	// blue 49-56
-	// line 8
+	//	offset 0xA8 - line 8
 	0x00, 0x00, 0x00, 0x00, 0x05, 0x17, 0x1F, 0x1F,	// red 57-64
 	0x1F, 0x17, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00,	// green 57-64
 	0x0F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x0F, 0x02,	// blue 57-64
+	//	offset 0xC0
+	0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7,
+	0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
+	//	offset 0xD0
+	0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7,
+	0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
+	//	offset 0xE0
+	0xE0, 0xE1, 0xE2,					// PORTA
+	0xE3, 0xE4, 0xE5,					// PORTB
+	0xE6, 0xE7, 0xE8,					// PORTC
+	0xE9, 0xEA, 0xEB,					// PORTD
+	0xEC,								// MCUSR
+	0xED,								// MCUCR
+	0xEE,								// PORTCR
+	0xEF,								// PRR
+	//	offset 0xF0
+	I2C_ID, VERSION,					// Who-Am-I and firmware version
+	0xF2,								// keys
+	0xF3,								// EEPROM write protect disabled
+	0xF4, 0xF5, 0xF6, 0xF7,
+	0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF,
 };
+//	some defines, to ease access
+#define pixels		registers[REG_PIXELS]
+#define keys		registers[REG_KEYS]
+#define watchdog	registers[REG_WDC]
 
-#ifndef NDEBUG
-volatile uint8_t registers[] = {
-	0x00, 0x01, 0x02,					// PORTA
-	0x03, 0x04, 0x05,					// PORTB
-	0x06, 0x07, 0x08,					// PORTC
-	0x09, 0x0A, 0x0B,					// PORTD
-	0x0C,								// MCUSR
-	0x0D,								// MCUCR
-	0x0E,								// PORTCR
-	0x0F,								// PRR
-};
-#endif
-
-volatile uint8_t keys		= 0xF2;		// joystick input status
-volatile uint8_t watchdog	= 0xF6;		// watchdog interrupt counter
 volatile uint8_t i2c_reg	= 0xFF;		// I2C register address
-volatile char i2c_busy		= 1;		// I2C transactions in progress
+volatile char i2c_busy		= 1;		// I2C transactions in progress - bit0 =active, bit1 =DATA is address
 volatile char redrawleds	= 1;		// redraw LEDs after pixels has been updated
 
 ISR(BADISR_vect, ISR_NAKED)
@@ -158,23 +190,23 @@ ISR(ANALOG_COMP_vect, ISR_ALIASOF(BADISR_vect));
 
 ISR(TIMER0_OVF_vect)
 {
+	//	without attribute ISR_NAKED registers get pushed, popped and RETI gets added
 	redrawleds	= 1;			// set flag to redraw LEDs
 	TIFR0		= 0;			// clear interrupt flag register
-	reti();						// return from interrupt and enable global interrupts
 }
 ISR(TIMER0_COMPA_vect, ISR_ALIASOF(TIMER0_OVF_vect));
 ISR(TIMER0_COMPB_vect, ISR_ALIASOF(TIMER0_OVF_vect));
 
 ISR(WDT_vect)
 {
+	//	without attribute ISR_NAKED registers get pushed, popped and RETI gets added
 	//	watchdog ISR is only used for wakeup
 	++watchdog;					// count
 	redrawleds	= 1;			// set flag to redraw LEDs
 	MCUSR	&= ~(1 << WDRF);	// clear watchdog reset flag
 	/*	WDIF is cleared by hardware when executing the corresponding interrupt handling vector.
-	WDTCSR	&= ~(1 << WDIF);	// clear interrupt flag
+	WDTCSR	|= (1 << WDIF);		// clear interrupt flag - with writing logic 1 to WDIF
 	*/
-	reti();						// return from interrupt and enable global interrupts
 }
 
 /*	defined in rpi-sense.S	ISR(TWI_vect, ISR_ALIASOF(BADISR_vect));*/
@@ -272,64 +304,75 @@ int main(void)
 	//	disable TIMER1,SPI,ADC clock for power reduction
 	PRR		= (0 << PRTWI) | (0 << PRTIM0) | (1 << PRTIM1) | (1 << PRSPI) | (1 << PRADC);
 
+	write_data((1 << 6), CONF_WRITE);		//	configure LED2472G shift register
+	/*	just for testing - little/big endian register alignment needs to be checked
+	read_data(CONF_READ);
+	asm volatile("STS registers+0xC0, r25");
+	asm volatile("STS registers+0xC1, r24");
+	asm volatile("STS registers+0xC2, r23");
+	asm volatile("STS registers+0xC3, r22");
+	*/
+
 	for(;;)
 	{
-		wdt_reset();		// reset watchdog timer
-		sei();				// Set Enable global Interrupts
+		wdt_reset();						// reset watchdog timer
+		sei();								// Set Enable global Interrupts
 
 		while(0 != i2c_busy)
 		{
 			//	just wait for I2C operation to finish
-			delay(200);		// (clkT0=8kHz, 200/8000=1/40s =25ms
+			delay(200);						// (clkT0=8kHz, 200/8000=1/40s =25ms
 		}
 
 #ifdef UNUSED_CODE
 		//clear_gain();
 		if(0 == redrawleds)
 		{
-			check_keys();	// skip LED update and only check joystick
+			check_keys();					// skip LED update and only check joystick
 		}
 		else
 #endif
 		{
-			draw_loop();	// copy pixels to LED shift register
+			draw_loop();					// copy pixels to LED shift register
 			// draw_loop always finishes with check_keys
 		}
 
+		registers[REG_EE_WP]	= ((PORTB & EE_WP) ?0 :1);	// copy EEPROM_WP disabled to I2C data buffer
+		registers[REG_WDTCSR]	= WDTCSR;	// copy register to I2C data buffer
+		registers[REG_SMCR]		= SMCR;		// copy register to I2C data buffer
 #ifndef NDEBUG
 		// copy registers to I2C data buffer
-		int pos = 0;
-		registers[pos++]	= PINA;
-		registers[pos++]	= DDRA;
-		registers[pos++]	= PORTA;
-		registers[pos++]	= PINB;
-		registers[pos++]	= DDRB;
-		registers[pos++]	= PORTB;
-		registers[pos++]	= PINC;
-		registers[pos++]	= DDRC;
-		registers[pos++]	= PORTC;
-		registers[pos++]	= PIND;
-		registers[pos++]	= DDRD;
-		registers[pos++]	= PORTD;
-		registers[pos++]	= MCUSR;
-		registers[pos++]	= MCUCR;
-		registers[pos++]	= WDTCSR;
-		registers[pos++]	= SMCR;
+		registers[REG_PINA]		= PINA;
+		registers[REG_DDRA]		= DDRA;
+		registers[REG_PORTA]	= PORTA;
+		registers[REG_PINB]		= PINB;
+		registers[REG_DDRB]		= DDRB;
+		registers[REG_PORTB]	= PORTB;
+		registers[REG_PINC]		= PINC;
+		registers[REG_DDRC]		= DDRC;
+		registers[REG_PORTC]	= PORTC;
+		registers[REG_PIND]		= PIND;
+		registers[REG_DDRD]		= DDRD;
+		registers[REG_PORTD]	= PORTD;
+		registers[REG_MCUSR]	= MCUSR;
+		registers[REG_MCUCR]	= MCUCR;
+		registers[REG_PORTCR]	= PORTCR;
+		registers[REG_PRR]		= PRR;
 #endif	// NDEBUG
 
 #ifdef USE_SLEEP
 		if(0 == i2c_busy && 0 == redrawleds)
 		{
 			// only if I2C operation ended and redraw unnecessary
-			sei();			// enable interrupts before SLEEP
-			SMCR	|= (1 << SE);	// enable SLEEP mode
-			sleep_cpu();	// sleep and wait for interrupt
-			SMCR	&= ~(1 << SE);	// clear SLEEP enable flag
+			sei();							// enable interrupts before SLEEP
+			SMCR	|= (1 << SE);			// enable SLEEP mode
+			sleep_cpu();					// sleep and wait for interrupt
+			SMCR	&= ~(1 << SE);			// clear SLEEP enable flag
 			_NOP();
-			_NOP();			// just to be sure a little delay after wake up from SLEEP
+			_NOP();							// just to be sure a little delay after wake up from SLEEP
 		}
 #endif
 	}
 
-	return(0);	// hopefully we never reach this
+	return(0);								// hopefully we never reach this
 }
